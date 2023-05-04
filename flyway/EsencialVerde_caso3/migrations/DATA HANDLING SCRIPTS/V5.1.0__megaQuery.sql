3f606e5863d0fe040fd1abcb0051a7636d227d24
Jose Granados <josepgs0929@gmail.com>
2023-05-04 05:33:51 -0600

    FEAT: definition of the megaQuery

--- flyway/EsencialVerde_caso3/migrations/DATA HANDLING SCRIPTS/V5.1.0__megaQuery.sql
+++ flyway/EsencialVerde_caso3/migrations/DATA HANDLING SCRIPTS/V5.1.0__megaQuery.sql
@@ -3,6 +3,9 @@
 -- Fecha: 30/04/2023
 -- Descripcion: select with structure like join, group by, order by, for json, except and others
 -----------------------------------------------------------
+-- Este query realiza una consulta a dos tablas, "contacts" e "invoices", y devuelve información sobre los contactos que han hecho compras en la tienda. Para ello, utiliza una operación de "left join" para relacionar las dos tablas por medio de un campo llamado "buyerContact" en la tabla de "invoices", que contiene el ID del contacto que realizó la compra.
+-- Luego, aplica dos funciones de agregación, "MAX" y "COUNT", para obtener la fecha de la última factura y el número total de facturas para cada contacto. Además, utiliza una cláusula "HAVING" para filtrar aquellos contactos que hayan realizado al menos una compra.
+-- Finalmente, utiliza la cláusula "EXCEPT" para obtener aquellos contactos que cumplan con los criterios anteriores pero que no hayan realizado ninguna compra en el año 2022. Y, si se aplica la cláusula "ORDER BY", ordena el resultado de la consulta en base al nombre del contacto.
 SELECT c.contactId, c.name, c.surname1, 
        MAX(i.postdate) AS lastInvoiceDate, 
        COUNT(i.invoiceId) AS totalInvoices,
