ab2ec546f0de6cc0cd501d97f1598a739580b977
Jose Granados <josepgs0929@gmail.com>
2023-06-11 17:17:24 -0600

    ADD: update report and compete select for the report

--- Caso4/userPermissions.sql
+++ Caso4/userPermissions.sql
@@ -45,3 +45,65 @@ GROUP BY
     wt.name
 ORDER BY
     [País o Región], p.name, wt.name
+
+
+    --- FUNCIONAL
+
+WITH treatmentCosts AS (
+    SELECT
+        wtm.wasteTypeId,
+        ctc.cost
+    FROM
+        countryTreatmentCost ctc
+        INNER JOIN wasteTypesXtreatmentMethods wtm ON ctc.treatmentMethodId = wtm.methodId
+)
+SELECT
+    co.name AS country,
+    com.companyName AS company,
+    wt.name AS wasteType,
+    SUM(con.amount) AS totalCollected,
+    SUM(ctc.cost * con.amount) AS totalCostProcessed,
+    SUM(ctc.cost * con.amount) - SUM(ctc.cost * con.amount * wt2.cost) AS netProfit
+FROM
+    companies com
+    INNER JOIN invoices con ON com.companyId = con.companyId
+    INNER JOIN contracts con2 ON con.invoiceId = con2.contractId
+    INNER JOIN countries co ON con2.countryId = co.countryId
+    INNER JOIN wasteTypes wt ON con2.wasteTypeId = wt.wasteTypeId
+    INNER JOIN countryTreatmentCost ctc ON co.countryId = ctc.countryId
+    LEFT JOIN treatmentCosts wt2 ON wt.wasteTypeId = wt2.wasteTypeId
+GROUP BY
+    co.name,
+    com.companyName,
+    wt.name;
+
+
+-- Funcional con fechas y power bi
+SELECT
+co.name AS country,
+com.companyName AS company,
+wt.name AS wasteType,
+i.postdate,
+SUM(i.amount) AS totalCollected,
+SUM(ctc.cost * i.amount) AS totalCostProcessed,
+SUM(ctc.cost * i.amount) - SUM(ctc.cost * i.amount * wt2.cost) AS netProfit
+FROM
+companies com
+INNER JOIN invoices i ON com.companyId = i.companyId
+INNER JOIN countries co ON com.countryId = co.countryId
+INNER JOIN wasteTypes wt ON i.wasteTypeId = wt.wasteTypeId
+INNER JOIN countryTreatmentCost ctc ON co.countryId = ctc.countryId
+LEFT JOIN (
+SELECT
+wtm.wasteTypeId,
+ctc.cost
+FROM
+countryTreatmentCost ctc
+INNER JOIN wasteTypesXtreatmentMethods wtm ON ctc.treatmentMethodId = wtm.methodId
+) wt2 ON wt.wasteTypeId = wt2.wasteTypeId
+GROUP BY
+co.name,
+com.companyName,
+wt.name,
+i.postdate
+
